9d608e02fca66835420e160ee2f920af9485034c
coco <mrkokipro@icloud.com>
2020-03-14 06:27:39 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/composition-api'
+import Buefy from 'buefy'
+import './assets/sass/global.sass'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fas'
+})
 
 new Vue({
   router,
