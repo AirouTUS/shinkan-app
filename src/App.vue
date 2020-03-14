61f201ce17c70cfdb0b13e62cb1f67880a204870
kevikinson <kevin.kougakubu@gmail.com>
2020-03-14 18:47:18 +0900

    footer 1.1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template lang="pug">
   div#app
     router-view
-    footer-nav
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import FooterNav from '@/components/FooterNav.vue'
-
 export default Vue.extend({
-  components:{FooterNav}
+
 })
 </script>
 
