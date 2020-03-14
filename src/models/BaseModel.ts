c81c7d9c9cde7d84be52c4fd2f7b450493da938b
coco <mrkokipro@icloud.com>
2020-03-14 18:27:06 +0900

    テストデータの作成

--- src/models/BaseModel.ts
+++ src/models/BaseModel.ts
@@ -1,4 +1,26 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
+import qs from 'qs'
 
 export default abstract class BaseModel {
+
+  static getHeaders() {
+    return {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+  }
+  static stringify = (query = {}) => qs.stringify(query, {indices: false})
+
+  protected axios = axios.create({
+    headers: BaseModel.getHeaders()
+  })
+
+  protected constructor(baseURL: string) {
+    this.axios = axios.create({baseURL, headers: BaseModel.getHeaders()})
+  }
+
+  public async get(id: string) {
+    return this.axios
+      .get(`/${id}`)
+  }
 }
