50f0775417853beffe87a5f730e8b89818ff15ba
coco <mrkokipro@icloud.com>
2020-03-14 16:56:42 +0900

    見た目のみ作成

---
+++ src/components/CategoryNav.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+  div#category-nav
+    nav
+      b-tabs(v-model="activeTab" position="is-centered")
+        b-tab-item(v-for="category in categories" :key="category.id" :label="category.name")
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+type Category = {
+  id: number,
+  name: string
+}
+type Tab = {
+  tab: number
+}
+
+const categories = [
+  {
+    id: 1,
+    name: '運動サークル'
+  }, {
+    id: 2,
+    name: '文化サークル'
+  }
+] as (Category & Tab)[] 
+
+export default Vue.extend({
+  data() {
+    return {
+      categories,
+      activeTab: 0
+    }
+  }
+})
+</script>
+
+<style lang="sass">
+  #category-nav
+    nav
+      position: fixed
+      top: 0
+      width: 100%
+      height: $header-nav-height
+      background: white
+      z-index: 9
+      > .b-tabs > .tabs
+        > ul
+          color: $text-gray
+          > li 
+            > a
+              color: $text-gray
+            &.is-active
+              > a
+                color: $main-color
+                border-bottom: 2px solid $main-color
+</style>
