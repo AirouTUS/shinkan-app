d0cad78e3fbfe7ae66a8fb51f3b88e7d93938a59
kevikinson <kevin.kougakubu@gmail.com>
2020-03-14 18:37:09 +0900

    footer tsuikashitade~~~

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template lang="pug">
   div#app app
     router-view
+    footer-nav
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import FooterNav from '@/components/FooterNav.vue'
+
 export default Vue.extend({
-  
+  components:{FooterNav}
 })
 </script>
 
--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -1,5 +1,13 @@
 <template lang="pug">
-  div#footer-nav 
+  div#wrapper
+    div#footer-nav
+      div#top-icon
+        img(src="@/assets/svg/CircleTop.svg")
+        p TUS新歓
+      div#list-icon
+        img(src="@/assets/svg/CircleList.svg")
+        div#kuuhaku
+          p サークル一覧
 </template>
 
 <script lang="ts">
@@ -9,5 +17,31 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-
+  #wrapper
+    position: fixed
+    bottom: 0
+  #footer-nav 
+    display: -webkit-flex
+    display: flex
+    align-items: stretch
+    position:fixed
+    font-family: Hiragino Kaku Gothic ProN
+    bottom: 0
+    background: white
+    width: 100vw
+    box-sizing: border-box
+    border-top: 1px solid gainsboro
+  #top-icon
+    color: #BBBBBB
+    text-align: center
+    font-size: 12px
+    margin-left: 24px
+  #list-icon
+    color: #A8DC39
+    text-align: center
+    font-size: 12px
+    margin-left: 19px
+    margin-top: 3px
+  #kuuhaku
+    margin-top: 7px
 </style>
