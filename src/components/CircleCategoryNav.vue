6ca2fb093b59552462e51baa7db81e08771c4cb4
shota8888 <asbscs@icloud.com>
2020-03-15 01:36:28 +0900

    サークルカテゴリーページに飛ぶボタンの作成

--- src/components/CircleCategoryNav.vue
+++ src/components/CircleCategoryNav.vue
@@ -1,13 +1,44 @@
 <template lang="pug">
-  
+	.CircleCategory
+		.line(:style="{ 'background-color': backgroundColor }")
+		.main_button(:style="{ 'background-color': backgroundColor }")
+			router-link.main_button_items(:to="link")
+				b-icon(:icon="iconName")
+				p {{ categoryTitle }}
+				b-icon(icon="angle-right")
+
 </template>
 
 <script lang="ts">
 export default {
-    
+    props: {
+			backgroundColor: String,
+			categoryTitle: String,
+			iconName: String,
+			link: String,
+			linkId: Number
+		}
 }
 </script>
 
 <style lang="sass" scoped>
+	.CircleCategory
+		.line
+			width: 100%
+			height: 8px
+			margin-bottom: -4px
+		.main_button
+			max-width: 125px
+			border-top-right-radius: 6px
+			border-bottom-right-radius: 6px
+			&_items
+				color: white
+				padding: 0 0 4px 4px
+				display: flex
+				flex-direction: row
+				align-items: center
+				p
+					margin-right: 10px 
+					font-size: 10px
 
 </style>
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   div#top
     top-section
+    circle-category-nav(link="/circles" iconName="running" categoryTitle="運動サークル" backgroundColor="orange")
+    circle-category-nav(link="/circles" iconName="running" categoryTitle="文化サークル" backgroundColor="blue")
     circle-list-section
 </template>
 
@@ -8,8 +10,9 @@
 import Vue from 'vue'
 import CircleListSection from '@/components/top/CircleListSection.vue'
 import TopSection from '@/components/top/TopSection.vue'
+import CircleCategoryNav from '@/components/CircleCategoryNav.vue'
 export default Vue.extend({
-  components: { CircleListSection, TopSection }
+  components: { CircleListSection, TopSection, CircleCategoryNav }
 })
 </script>
 
