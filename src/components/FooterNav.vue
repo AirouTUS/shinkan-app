70640c999272d027d05fb48739546f27b47fa07b
CutRay <zeron9421@gmail.com>
2020-03-14 18:51:49 +0900

    ページ遷移作成中

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -1,13 +1,35 @@
 <template lang="pug">
-  div#footer-nav 
+  div#footer-nav
+    div(:class="{ active: pageName==='top' }" @click="changePage('top')") ページ1
+    div(:class="{ active: pageName==='circles' }" @click="changePage('circles')") ページ2
+    img(src="@/assets/svg/CircleList.svg")
 </template>
 
 <script lang="ts">
-export default {
-
+import Vue from "vue"
+export type DataType = {
+    pageName: string
 }
+export default Vue.extend({
+    data(): DataType {
+      return {
+        pageName:'top'
+      }
+    },
+    methods: {
+      changePage(page:string):void{
+        alert(page)
+        this.pageName=page
+        console.log(`${location.host}/${page}`)
+        // window.location.href=`${location.hostname} /${page}`
+      }
+    }
+})
 </script>
 
 <style lang="sass" scoped>
-
+  #footer-nav
+    img
+      fill: red
+    
 </style>
