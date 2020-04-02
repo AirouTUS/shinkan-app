137d508b4c97564225c8381016b33474c7b4b358
coco <mrkokipro@icloud.com>
2020-04-02 20:44:18 +0900

    twitterとmailの実装

--- src/views/Circle.vue
+++ src/views/Circle.vue
@@ -1,41 +1,43 @@
 <template lang="pug">
-  div#page-circle.bg-default
-    div.page-wrapper
+  div#page-circle.page.bg-default
+    div.circle-header.p-3.flexbox.has-space-between
+      back-button
+      share-button(:title="share.title" :text="share.text" :url="share.url")
 
-      div.circle-header.p-3.flexbox.has-space-between
-        back-button
-        share-button(:title="share.title" :text="share.text" :url="share.url")
+    b-carousel.circle-images(:autoplay="true" :has-drag="true" :indicator="false")
+      b-carousel-item(v-for="(image, i) in (circle.images && circle.images.length > 0) ? circle.images : [{}]" :key="i")
+        v-img.circle-image(:src="image.url")
 
-      b-carousel.circle-images(:autoplay="true" :has-drag="true" :indicator="false")
-        b-carousel-item(v-for="(image, i) in (circle.images && circle.images.length > 0) ? circle.images : [{}]" :key="i")
-          v-img.circle-image(:src="image.url")
+    div.circle-eyecatch
+      v-img(:src="circle.eyecatch")
+    div.circle-title.flexcolumn.is-middle
+      h1.bold.is-size1.pt-3 {{ circle.name }}
+      p.mt-3(v-if="circle.catchCopy") {{ circle.catchCopy }}
+      div.flexbox.mt-3(v-if="circle.twitter || circle.email")
+        a.sns-icon.mx-2(v-if="circle.twitter" :href="`https://twitter.com/${circle.twitter}`" target="_blank" rel="noopener noreferrer")
+          img(src="/img/twitter.png")
+        a.sns-icon.mx-2(v-if="circle.email" :href="`mailto:${circle.email}`")
+          img(src="/img/envelope.png")
+      
+    div.page-content.text-gray.bold
+      p(v-if="circle.about")
+        b-icon(icon="hand-point-right")
+        span.ml-3 {{ circle.about }}
+      p.pt-3(v-if="circle.location")
+        b-icon(icon="thumbtack")
+        span.ml-3 {{ circle.location }}
+      p.pt-3(v-if="circle.workTime")
+        b-icon(icon="clock")
+        span.ml-3 {{ circle.workTime }}
+      p.pt-3(v-if="circle.members_number")
+        b-icon(icon="users")
+        span.ml-3 {{ circle.members_number }}
+      p.pt-3(v-if="circle.cost")
+        b-icon(icon="dollar-sign")
+        span.ml-3 {{ circle.cost }}
 
-      div.circle-eyecatch
-        v-img(:src="circle.eyecatch")
-
-      div.circle-title.flexcolumn.is-middle
-        h1.bold.is-size1.pt-3 {{ circle.name }}
-        p.mt-3(v-if="circle.catchCopy") {{ circle.catchCopy }}
-        
-      div.circle-container.text-gray.bold
-        p(v-if="circle.about")
-          b-icon(icon="hand-point-right")
-          span.ml-3 {{ circle.about }}
-        p.pt-3(v-if="circle.location")
-          b-icon(icon="thumbtack")
-          span.ml-3 {{ circle.location }}
-        p.pt-3(v-if="circle.workTime")
-          b-icon(icon="clock")
-          span.ml-3 {{ circle.workTime }}
-        p.pt-3(v-if="circle.members_number")
-          b-icon(icon="users")
-          span.ml-3 {{ circle.members_number }}
-        p.pt-3(v-if="circle.cost")
-          b-icon(icon="dollar-sign")
-          span.ml-3 {{ circle.cost }}
-
-      div.circle-container(v-html="sanitize(circle.description)")
-        p(v-html="sanitize(circle.description)")
+    div.page-content(v-html="sanitize(circle.description)")
+      p(v-html="sanitize(circle.description)")
 </template>
 
 <script lang="ts">
@@ -107,15 +109,25 @@ export default defineComponent({
 </style>
 
 <style lang="sass" scoped>
+  .sns-icon
+    width: 24px
+    height: 24px
+    .twitter
+      background: url(/svg/envelop.svg)
+    .email
+      background: url(/svg/email.svg)
   .action
     color: white
     border-radius: 100px
     background: rgba(0, 0, 0, 0.5)
     box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.25)
-  .page-wrapper
+
+  .page
     padding-bottom: 40px
-  .circle-container
-    padding: 16px
+    &-content
+      padding: 8px 16px 
+      padding-bottom: 16px
+
   .circle
     position: relative
     &-header
